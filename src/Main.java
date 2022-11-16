daa5b84f3f1c12a1e17d56bdc58f3cee42517f79
Василий <remindscope@gmail.com>
2022-11-16 15:47:27 +0300

    InitialCommit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // =====Task 1=====
+
+        System.out.println("=====Task 1=====");
+        // iOs = 0, Android = 1
+        byte clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Install Android App via link");
+        } else
+            System.out.println("Install iOs app via link");
+
+        // =====Task 2=====
+
+        System.out.println("=====Task 2=====");
+        short clientDeviceYear = 2015;
+        if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Install Android App via link");
+            } else {
+                System.out.println("Install light Android App via link");
+            }
+        } else if (clientDeviceYear >= 2015) {
+            System.out.println("Install iOs app via link");
+        } else System.out.println("Install light iOs App via link");
+
+        // =====Task 3=====
+
+        System.out.println("=====Task 3=====");
+        short year = 2021;
+        if (year % 100 == year % 400 && year % 4 == 0) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is a non-leap year");
+        }
+
+        //=====Task 4=====
+
+        System.out.println("=====Task 4=====");
+        int deliveryDistance = 95;
+        int daysAmount = 1 + (deliveryDistance + 19) / 40;
+        System.out.println("Delivery days: " + daysAmount);
+
+        //=====Task 5=====
+
+        System.out.println("=====Task 5=====");
+        byte monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("This month doesn't exist");
+        }
+
+    }
+
+}
